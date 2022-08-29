8a6582fd4db01197f17bc51f2638546e168df7ff
benjamin <b.james.neustadt@gmail.com>
2022-08-29 16:50:08 +0200

    Refactor: use eval() instead of parseInt()
    * Red Green _Refactor_

--- checkMath.js
+++ checkMath.js
@@ -1,10 +1,8 @@
 const checkMath = (n) => {
   if (n.length == 1) {
     return [n, parseInt(n)]
-  } else if (n.includes('-')) {
-    return ["2 - 1", 1.0];
   } else {
-    const result = [n, ((parseInt(n[0])) + (parseInt(n[4])))];
+    const result = [n, (eval(n))];
     return result;
   }
 }
