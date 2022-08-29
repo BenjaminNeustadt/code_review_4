ab99074273e8fbd1885e61781f85ee49524cc429
benjamin <b.james.neustadt@gmail.com>
2022-08-29 13:30:10 +0200

    Add: second test for addition

--- checkMath.js
+++ checkMath.js
@@ -1,5 +1,9 @@
-const checkMath = () => {
-  return ["1", 1.0];
+const checkMath = (n) => {
+  if (n == "1"){
+    return ["1", 1.0];
+  } else {
+    return ["1 + 1", 2.0];
+  }
 }
 
 module.exports = checkMath;
--- checkMath.test.js
+++ checkMath.test.js
@@ -4,6 +4,10 @@ describe ('checkMath', () => {
   it('returns a single number', () => {
     expect(checkMath("1")).toStrictEqual(["1", 1.0])
   })
+  
+  it('returns a single number', () => {
+    expect(checkMath("1 + 1")).toStrictEqual(["1 + 1", 2.0])
+  })
 
 
 })
